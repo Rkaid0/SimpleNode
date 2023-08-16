81c66391e3e3b4c7f53783d9211f8d23ca241eb9
Artyom Yesayan <yesart8@gmail.com>
2023-08-16 14:52:57 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", 'r') as file:
 setup(
   name = 'SimpleNode',
   packages = ['SimpleNode'],
-  version = '0.4',
+  version = '0.4a1',
   license='MIT',
   description = 'Package for working with Neural Networks for small projects',
   long_description_content_type = "text/markdown",
