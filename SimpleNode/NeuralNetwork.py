286d07e53a840e774d9aee5a436b8e0ee19b8475
Artyom Yesayan <yesart8@gmail.com>
2023-08-16 14:28:10 -0400

    Updated NeuralNetwork.py

--- SimpleNode/NeuralNetwork.py
+++ SimpleNode/NeuralNetwork.py
@@ -1,8 +1,8 @@
 import numpy as np
 import json
 
-import ActivationFunctions
-import Layer
+from SimpleNode.ActivationFunctions import ActivationFunctions
+from SimpleNode.Layer import Layer
     
 class NeuralNetwork:
     def __init__ (self, layerLayout, activationFunctionClass):
