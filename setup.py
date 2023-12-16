1a97641ab5a67a93e72f6315a4b4571ceed30d1d
Rkaid0 <99305396+Rkaid0@users.noreply.github.com>
2023-12-16 16:07:47 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", 'r') as file:
 setup(
   name = 'SimpleNode',
   packages = ['SimpleNode'],
-  version = '0.4a4',
+  version = '0.31',
   license='MIT',
   description = 'Package for working with Neural Networks for small projects',
   long_description_content_type = "text/markdown",
